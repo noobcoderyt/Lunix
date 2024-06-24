72945cf28eea52e5ac7a7ff970dfc2c3e3899f7f
NoobCoder <107041625+noobcoderyt@users.noreply.github.com>
2024-06-24 20:14:04 +0530

    Added comic, fact and joke commands

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import random
 import aiohttp
 import json
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -37,6 +38,9 @@ async def info(ctx):
         .prowler
         .calculate equation
         .meme
+        .comic
+        .joke
+        .fact
                     """, inline=False)
     embed.add_field(name="", value="I will also reply to your messages occasionally!")
 
@@ -76,7 +80,7 @@ async def flip(ctx):
     time.sleep(0.5)
     await ctx.send("...")
     time.sleep(0.5)
-    await ctx.send(f"{random.choice(coin)}!!!")
+    await(f"{random.choice(coin)}!!!")
 
 @bot.command()
 async def roll(ctx):
@@ -90,7 +94,7 @@ async def roll(ctx):
     time.sleep(0.5)
     await ctx.send("...")
     time.sleep(0.5)
-    await ctx.send(f"{random.randint(1,6)}!!!")
+    await(f"{random.randint(1,6)}!!!")
 
 @bot.command()
 async def wish(ctx, arg):
@@ -104,6 +108,16 @@ async def wish(ctx, arg):
 @bot.command()
 async def kamehameha(ctx):
     await ctx.send(random.choice(kamehamehagifs))
+
+@bot.command()
+async def calculate(ctx, arg):
+    if arg == "":
+        await ctx.send("I can't calculate nothing")
+    else:
+        try:
+            await ctx.send(eval(arg))
+        except:
+            await ctx.send("Nah Im not that good")
     
 @bot.command(name="8ball")
 async def _8ball(ctx, arg):
@@ -124,5 +138,77 @@ async def meme(ctx):
                 await ctx.send(embed=embed)
             else:
                 await ctx.send('Error 404: Meme not found')
+
+@bot.command()
+async def comic(ctx):
+    response = requests.get('https://xkcd.com/info.0.json')
+    data = response.json()
+    comic_title = data['title']
+    comic_url = data['img']
+    embed = discord.Embed(title=comic_title, color=discord.Colour.blue())
+    embed.set_image(url=comic_url)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def joke(ctx):
+    response = requests.get("https://v2.jokeapi.dev/joke/Any")
+    data = response.json()
+    await ctx.send(f"{data['setup']} - {data['delivery']}")
+
+@bot.command()
+async def fact(ctx):
+    response = requests.get("https://api.api-ninjas.com/v1/facts", headers={'X-Api-Key': 'KEY'})
+    fact_data = json.loads(response.text)
+    fact_text = fact_data[0]['fact']
+    await ctx.send(fact_text)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if "kys" in message.content.lower() or message.content.startswith("NI"):
+        await message.reply("stfu _ who r u")
+
+    if "socket" in message.content.lower():
+        await message.reply("Socket more like Cocket lmfao")
+
+    if "scarry" in message.content.lower():
+        await message.reply("Mf be like I have a high end pc while running games in 20fps with 240p")
+
+    if ":trollface:" in message.content.lower():
+        await message.channel.send("<:ohwel:1212344068911271936>")
+    
+    if "hard" in message.content.lower():
+        await message.reply("thats what she said")
+
+    if "gato" in message.content.lower():
+        for i in range(random.randint(1,9)):
+            await message.channel.send("GATO IS BACK")
+            time.sleep(0.5)
+        await message.channel.send("<:tr:1248294470588563497>")
+
+    if "improved" in message.content.lower():
+        await message.reply("<:lie:1220656617691938817>")
+
+    if "new video" in message.content.lower():
+        await message.reply("maybe the new video was the friends we made along the way")
+
+    if "mint" in message.content.lower():
+        await message.channel.send("Hell yeah Linux Mint")
+
+    if message.content.startswith("?ban <@1126807595517227089>"):
+        await message.reply("Major Skill Issue detected ⚠️")
+
+    if "<@1253260636662792213>" in message.content:
+        await message.channel.send("I have been summoned")
+
+    if "mrace" in message.content.lower():
+        await message.reply("Mr ace more like Mr ass lmfaooooo")
+
+    if "discox" in message.content.lower():
+        await message.reply(f"Congratulations {message.author}! You are now *an* Member!")
+
+    await bot.process_commands(message)
 
 bot.run("TOKEN")
