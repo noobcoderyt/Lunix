4a86a7517b713587133747f7829605772397caa8
poopKiller <139982785+MrAce-Gaming@users.noreply.github.com>
2024-06-27 20:05:36 +0530

    Updated 8BALL (check)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ intents.members = True
 bot = commands.Bot(command_prefix=".", intents=intents, help_command=None)
 
 coin = ["HEAD", "TAILS"]
-_8ball_ = ["very", "NAWWWWW"]
+_8ball_ = ["very", "NAWWWWW", "i think", "💀", ":sadhampter:"]
 kamehamehagifs = ["https://c.tenor.com/yOej10JYX4sAAAAM/kamehameha-ui-goku.gif","https://66.media.tumblr.com/40e19795a78fbb4dc5212b0416870bad/tumblr_p262c08aKm1wyh2j4o1_500.gif","https://media1.tenor.com/images/8f7b25ee13cfd669418c78cd50431de3/tenor.gif?itemid=11539971","https://4.bp.blogspot.com/-E_BQvOD2TsM/WNKLRZPg3MI/AAAAAAAAZZ8/hdfcuVeBjp88nJQmON6tJyTDvXZhuQtBwCLcB/s1600/Gifs+animados+Kamehameha+11.gif", "https://media.tenor.com/images/be06b296f1144d9d37dadbd22f46cf54/tenor.gif"]
 
 
