adfba68196fa6241b7766b685797252a2a088e57
Marce1 <76426617+eMarce1@users.noreply.github.com>
2024-06-27 19:38:39 +0300

    Unmute

--- main.py
+++ main.py
@@ -490,10 +490,10 @@ async def mute(ctx, member: discord.Member = None, duration: str = None, *, reas
                               colour=0x00b0f4)
 
         embed.add_field(name="Usage",
-                        value="`.mute <member> <duration> [reason]`\n*<member>* - The member you want to softban\n*<duration>* - The duration of the mute.\n*[reason]* - The reason for the softban (optional)",
+                        value="`.mute <member> <duration> [reason]`\n*<member>* - The member you want to mute\n*<duration>* - The duration of the mute.\n*[reason]* - The reason for the mute (optional)",
                         inline=False)
         embed.add_field(name="Example",
-                        value="`.mute noobcoderyt Loser`\n\n*Noobcoder* will be softban with the reason *Loser*",
+                        value="`.mute noobcoderyt Loser`\n\n*Noobcoder* will be muted with the reason *Loser*",
                         inline=False)
 
         await ctx.send(embed=embed)
@@ -522,7 +522,7 @@ async def mute(ctx, member: discord.Member = None, duration: str = None, *, reas
         timeout_duration = timedelta(seconds=delay) if delay else None
         await member.edit(timed_out_until=discord.utils.utcnow() + timeout_duration, reason=reason)
         embed = discord.Embed(title="Muted!",
-                              description=f"{member.mention} has been soft muted!",
+                              description=f"{member.mention} has been muted!",
                               colour=0x00c105)
 
         embed.add_field(name="Reason",
@@ -532,6 +532,71 @@ async def mute(ctx, member: discord.Member = None, duration: str = None, *, reas
         embed.add_field(name="Duration",
                         value=f"`{duration}`",
                         inline=True)
+
+        await ctx.send(embed=embed)
+
+    except discord.Forbidden:
+        embed = discord.Embed(title="Error!",
+                              description="A permission related error has occured.",
+                              colour=0xff0000)
+
+        await ctx.send(embed=embed)
+
+    except discord.HTTPException as e:
+        embed = discord.Embed(title="Error!",
+                              description="An unknown error has occured",
+                              colour=0xff0000)
+
+        embed.add_field(name="Error code",
+                        value=f"`{e}`",
+                        inline=False)
+
+        await ctx.send(embed=embed)
+
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def unmute(ctx, member: discord.Member = None, *, reason: str = None):
+
+
+    if member is None:
+        embed = discord.Embed(title="Unmute",
+                              description="The unmute command can be used to unmute a user",
+                              colour=0x00b0f4)
+
+        embed.add_field(name="Usage",
+                        value="`.unmute <member> <duration> [reason]`\n*<member>* - The member you want to unmute\n*[reason]* - The reason for the unmute (optional)",
+                        inline=False)
+        embed.add_field(name="Example",
+                        value="`.unmute noobcoderyt  Not loser`\n\n*Noobcoder* will be unmuted with the reason *Not loser*",
+                        inline=False)
+
+        await ctx.send(embed=embed)
+        return
+        return
+
+    if member.top_role >= ctx.author.top_role:
+        embed = discord.Embed(title="Error!",
+                              description="You can't unmute this user.",
+                              colour=0xff0000)
+
+        await ctx.send(embed=embed)
+        return
+        return
+
+
+
+    if reason is None:
+        reason = "No reason specified"
+
+    try:
+        await member.edit(timed_out_until=None, reason=reason)
+        embed = discord.Embed(title="Unmuted!",
+                              description=f"{member.mention} has been unmuted!",
+                              colour=0x00c105)
+
+        embed.add_field(name="Reason",
+                        value=f"`{reason}`",
+                        inline=False)
 
         await ctx.send(embed=embed)
 
